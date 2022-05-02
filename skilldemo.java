ab0c748d43c8961bcb0316b9570a362e09539500
KristinEbu <ebuengankris@gmail.com>
2022-05-01 21:54:50 -0700

    test fails

---
+++ skilldemo.java
@@ -0,0 +1,5 @@
+public class skilldemo{
+    static int perimeter(int length, int width){
+        return (length + width)*2;
+    }
+}
---
+++ skilldemotest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemotest {
+    @Test
+    public void testPerimeter(){
+        assertEquals(10, skilldemo.perimeter(2,2));
+    }
+}
